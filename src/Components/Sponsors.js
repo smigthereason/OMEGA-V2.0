1c281c0733c8d89237bcf15d1c502890dc4265d2
victor@victor <victor.dmaina@gmail.com>
2024-05-13 02:12:07 +0300

    Reconfigure css and add components

--- src/Components/Sponsors.js
+++ src/Components/Sponsors.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Sponsor = () => {
   return (
-    <div className="container mx-auto mt-4 bg-black rounded-lg">
+    <div className="container mx-auto mt-4 bg-black rounded-lg h-80">
       <div className="flex gap-5">
-        <div className="flex-row text-white text-center border border-current rounded-bl-xl rounded-tr-xl">
-          <p className="vertical-text rotate-180 px-1 py-1 text-center mt-12">GRATITUDE</p>
+        <div className="flex-row text-white text-center border border-current rounded-bl-xl rounded-tr-xl mt-8">
+          <p className="vertical-text rotate-180 px-1 py-1 text-center mt-8 mb-4">GRATITUDE</p>
         </div>
-        <div className=" pr-4  text-white w-full text-pretty text-center px-4 py-4">
+        <div className=" pr-4  text-white w-full text-pretty text-center px-4 py-4 mt-8">
           <p className="text-lg w-full italic font-semibold mb-4" >
             We extend our deepest gratitude and appreciation to our esteemed sponsors who have played a pivotal role in the success and growth of Omega. Your unwavering support, commitment, and belief in our vision have been instrumental in propelling us forward. Through your generous contributions and strategic partnerships, we have been able to innovate, expand our reach, and achieve significant milestones. Your investment in our endeavors has not only benefited Omega but has also created a positive impact on our industry and community. Thank you for being an integral part of the Omega family and for your continued support.
           </p>
